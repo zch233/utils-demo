905f3e901830ec17c2b6954c1861c57ac876c1ae
张超 <529743595@qq.com>
2023-11-13 16:59:34 +0800

    feat: add hooks useCountDown

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -1,1 +1,16 @@
 export const inBrowser = typeof window !== 'undefined';
+
+export function raf(fn: FrameRequestCallback): number {
+    return inBrowser ? requestAnimationFrame(fn) : -1;
+}
+
+export function cancelRaf(id: number) {
+    if (inBrowser) {
+        cancelAnimationFrame(id);
+    }
+}
+
+// double raf for animation
+export function doubleRaf(fn: FrameRequestCallback): void {
+    raf(() => raf(fn));
+}
