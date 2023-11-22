56d26a94df8760f173819ffcd931144168720ba5
张超 <529743595@qq.com>
2023-11-22 15:17:59 +0800

    feat: add downloadWith

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { set } from 'lodash-es';
+import type { AxiosResponse } from 'axios';
+
 // https://release.group-ds.com/dev-newbee-handbook/utils/utils.html#base64ToFile
 export const base64ToFile = (base64: string, filename?: string) => {
     let arr = base64.split(','),
@@ -14,3 +17,46 @@ export const base64ToFile = (base64: string, filename?: string) => {
 // https://release.group-ds.com/dev-newbee-handbook/utils/utils.html#blobToFile
 export const blobToFile = (blob: Blob, filename?: string, options?: FilePropertyBag) =>
     new File([blob], filename || Date.now().toString(), { type: blob.type, ...options });
+
+// https://release.group-ds.com/dev-newbee-handbook/utils/utils.html#openLinkViaAnchor
+export const openLinkViaAnchor = (url: string, anchorAttrs?: Record<string, any>) => {
+    const anchor = document.createElement('a');
+    const $body = document.body;
+    anchor.href = url;
+    anchor.style.display = 'none';
+    if (anchorAttrs) Object.keys(anchorAttrs).map(key => set(anchor, key, anchorAttrs[key]));
+    $body.appendChild(anchor);
+    anchor.click();
+    $body.removeChild(anchor);
+};
+
+// https://release.group-ds.com/dev-newbee-handbook/utils/utils.html#openLink
+export const openLink = (url: string, target?: Record<string, any> | string, windowFeatures?: string) => {
+    const windowOpen = window.open(url, typeof target === 'string' ? target : undefined, windowFeatures);
+    if (windowOpen == null || typeof windowOpen === 'undefined') {
+        openLinkViaAnchor(url, typeof target === 'string' ? { target } : target);
+    }
+};
+
+// https://release.group-ds.com/dev-newbee-handbook/utils/utils.html#downloadWithBlob
+export const downloadWithBlob = (_response: AxiosResponse | Blob, filename?: string) => {
+    const blobData = (_response as AxiosResponse).data || _response;
+    return new Promise((resolve, reject) => {
+        if (blobData instanceof Blob) {
+            const fileReader = new FileReader();
+            fileReader.onload = function () {
+                const blob = blobData;
+                // 本地保存文件
+                const url = window.URL.createObjectURL(blob) || window.webkitURL.createObjectURL(blob);
+                // TODO：注意这里的 headers 用于获取文件名称，确保后台已经返回正确字段
+                const _filename = (_response as AxiosResponse).headers?.['content-disposition']?.split('filename*=')?.[1]?.substr(7);
+                openLinkViaAnchor(url, { download: decodeURIComponent(filename || _filename) });
+                resolve(blobData);
+            };
+            fileReader.readAsText(blobData);
+        } else {
+            console.error('downloadWithBlob：非文件流响应');
+            reject(_response);
+        }
+    });
+};
